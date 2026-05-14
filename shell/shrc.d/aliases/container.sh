e5439e28f031476341520491e498bdbf80f2ddba
Jack Kibort <jkibort928@gmail.com>
2026-05-14 11:29:38 -0700

    ctop

--- shell/shrc.d/aliases/container.sh
+++ shell/shrc.d/aliases/container.sh
@@ -3,3 +3,9 @@ alias enter='distrobox enter dev'
 
 # Shortcut
 alias db='distrobox'
+
+# Monitoring
+alias ctop='podman run --rm -it \
+    --security-opt label=disable \
+    -v "${XDG_RUNTIME_DIR}/podman/podman.sock:/var/run/docker.sock:Z" \
+    quay.io/vektorlab/ctop:latest'
